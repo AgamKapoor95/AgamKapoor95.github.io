67031b6839e0541c0ed2c9d6b7250eff9f95ea46
AgamKapoor95 <agamkapoor.95@gmail.com>
2023-01-25 13:26:51 +0530

    modification in portfolio

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,118 +1,134 @@
-import { Card, Container, HStack, Spacer, Icon, Show, Hide, Drawer, DrawerOverlay, DrawerContent, DrawerHeader, DrawerBody, Stack, useDisclosure, VStack } from '@chakra-ui/react'
-import Styles from "./navbar.module.css"
-import { BiMoon } from "react-icons/bi"
-import { RxHamburgerMenu } from "react-icons/rx"
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-
-const Navbar = () => {
-
-  let Links = [
-    {
-      path: "/",
-      label: "Home"
-    },
-    {
-      path: "/about",
-      label: "About"
-    },
-    {
-      path: "/skills",
-      label: "Skills"
-    },
-    {
-      path: "/projects",
-      label: "Projects"
-    },
-    {
-      path: "/contact",
-      label: "Contact"
-    },
-    {
-      path: "/resume",
-      label: "Resume"
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  useDisclosure,
+  Image,
+} from '@chakra-ui/react';
+import { Link } from 'react-scroll';
+import Agam_Kapoor_Resume from "../assets/Agam_Kapoor_Resume.pdf"
+import { ImFolderDownload } from "react-icons/im";
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { useEffect, useState } from 'react';
+export default function Navbar() {
+  const { isOpen, onToggle } = useDisclosure();
+  const [sticky, setSticky] = useState(false)
+  useEffect(()=>{
+    const handleScroll=()=>{
+      setSticky(window.scrollY>10)
     }
-  ]
-
+    window.addEventListener("scroll",handleScroll)
+  })
   return (
-    <Card bg="#FBFDFE" p={4} color="black" position="fixed" w="100%">
-      <Container maxW="80%">
-        <HStack>
-          <NavLink>Agam Kapoor</NavLink>
-          <Spacer />
-          <HStack gap={4}>
-            <Show above='md'>
-              {
-                Links.map((item, index) => {
-                  return <NavLink style={({ isActive }) => isActive ? { color: "#FF597B" } : { color: "black" }} className={Styles.navbar_links} key={index} to={item.path}>{item.label}</NavLink>
-                })
-              }
-            </Show>
+    <Box position="sticky" top="0" zIndex="2" >
+      <Flex
+        boxShadow="black 0px 3px 8px"
+        backgroundColor="#001c29"
+        minH={'70px'}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        align={'center'}>
+        <Flex
+          flex={{ base: 1, md: 'auto' }}
+          ml={{ base: -2 }}
+          display={{ base: 'flex', md: 'none' }}>
+          <IconButton
+            onClick={onToggle}
+            icon={
+              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+            }
+            variant='outline'
+            colorScheme="#64FFDA"
+            _hover={{
+              bg: 'teal.700',
+            }}
+          />
+        </Flex>
+        {/* <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+          <Image 
+            boxSize='50px'
+            objectFit='cover'
+            src={portfolio_logo} />
 
-            <NavLink> <Icon as={BiMoon} /></NavLink>
-            <Show below='md'>
-              <MobileNav />
-            </Show>
-          </HStack>
-        </HStack>
-      </Container>
-    </Card>
-  )
+        </Flex> */}
+
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={'flex-end'}
+          direction={'row'}
+          spacing={6}>
+          <Flex display={{ base: 'none', md: 'flex' }}>
+            <DesktopNav />
+          </Flex>
+          <a href={Agam_Kapoor_Resume} download="Agam_Kapoor_Resume.pdf">
+          <Button
+            display={{ base: 'none', md: 'inline-flex' }}
+            variant='outline'
+            colorScheme="#64FFDA"
+            _hover={{
+              bg: 'teal.700',
+            }}>
+            Resume <Text ml="10px" ><ImFolderDownload /></Text>
+          </Button>
+          </a>
+        </Stack>
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav />
+      </Collapse>
+    </Box>
+  );
 }
 
-export default Navbar
-
-
-export const MobileNav = () => {
-
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const [placement, setPlacement] = React.useState('right')
-
-  let Links = [
-    {
-      path: "/",
-      label: "Home"
-    },
-    {
-      path: "/about",
-      label: "About"
-    },
-    {
-      path: "/skills",
-      label: "Skills"
-    },
-    {
-      path: "/projects",
-      label: "Projects"
-    },
-    {
-      path: "/contact",
-      label: "Contact"
-    },
-    {
-      path: "/resume",
-      label: "Resume"
-    }
-  ]
+const DesktopNav = () => {
 
   return (
-    <>
-      <RxHamburgerMenu onClick={onOpen} />
-      <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerHeader borderBottomWidth='1px'>Atul Singh</DrawerHeader>
-          <DrawerBody>
-            <VStack>
-              {
-                Links.map((item, index) => {
-                  return <NavLink style={({ isActive }) => isActive ? { color: "#FF597B" } : { color: "black" }} className={Styles.navbar_links} key={index} to={item.path}>{item.label}</NavLink>
-                })
-              }
-            </VStack>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </>
-  )
-}
+    <Stack direction={'row'} spacing={2}>
+      <Link style={{width:"120px"}} to="home" spy={true} smooth={true} offset={-120} duration={500}>Home</Link>
+      <Link style={{width:"120px"}} to="about" spy={true} smooth={true} offset={-40} duration={500}>About Me</Link>
+      <Link style={{width:"120px"}} to="projects" spy={true} smooth={true} offset={-40} duration={500}>Projects</Link>
+      <Link style={{width:"120px"}} to="skills" spy={true} smooth={true} offset={-40} duration={500}>Skills</Link>
+      <Link style={{width:"120px"}} to="contact" spy={true} smooth={true} offset={-40} duration={500}>Contact Me</Link>
+    </Stack>
+  );
+};
+
+const MobileNav = () => {
+  return (
+    <Stack
+      p={4}
+      w={{base:"40%",sm:"30%"}}
+      marginBottom="20px"
+      borderBottom="1px solid #64FFDA"
+      borderRight="1px solid #64FFDA"
+      borderTop="1px solid #000f1670"
+      backgroundColor="#001c29"
+      fontSize={{base:"12px",sm:"15px", md:"17px"}}
+      display={{ md: 'none' }}>
+          <Flex flexDirection="column" textAlign="start" >
+            <Link style={{margin:"10px 0px"}} to="home" spy={true} smooth={true} offset={-70} duration={500}>Home</Link>
+            <Link style={{margin:"10px 0px"}} to="about" spy={true} smooth={true} offset={-20} duration={500}>About Me</Link>
+            <Link style={{margin:"10px 0px"}} to="projects" spy={true} smooth={true} offset={-70} duration={500}>Projects</Link>
+            <Link style={{margin:"10px 0px"}} to="skills" spy={true} smooth={true} offset={-70} duration={500}>Skills</Link>
+            <Link style={{marginBottom:"20px"}} to="contact" spy={true} smooth={true} offset={-40} duration={500}>Contact Me</Link>
+          </Flex>
+      <a href={Agam_Kapoor_Resume} download="Agam_Kapoor_Resume.pdf">
+        <Button
+          display='flex'
+          variant='outline'
+          colorScheme="#64FFDA"
+          fontSize={{base:"12px",sm:"15px", md:"17px"}}
+          _hover={{
+            bg: 'teal.700',
+          }}>
+          Resume
+        </Button>
+      </a>
+    </Stack>
+  );
+};
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,161 @@
+import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react'
 import React from 'react'
-import {Box} from "@chakra-ui/react"
+import { FaExternalLinkAlt } from 'react-icons/fa'
+import { SiGithub } from 'react-icons/si'
+import youtube from "../assets/youtube.png"
+// import acras from "../assets/acras.png"
+// import cricbuzz from "../assets/cricbuzz.png"
+// import trademart from "../assets/trademart.png"
 
 const Projects = () => {
   return (
-    <Box border="1px solid" minH="100vh">Projects</Box>
+    <Box className='projects' p={{base:"5px 0px", md:"50px 0px"}} >
+      
+      <Heading pb={{base:"20px", md:"50px"}} >Projects</Heading>
+      <Box width={{base:"100%",sm:"90%",lg:"70%"}} m="auto" >
+
+  
+        <Box position= "relative" width= {{base:"100%",md:"70%"}} height= {{base:"250px",md:"300px"}} mb={{base:"300px", md:"10%"}}  >
+          <Image src={youtube} objectFit="cover" w="100%" height={{base:"80%", sm:"100%"}} borderRadius="10px" boxShadow="black 0px 5px 15px"/>
+
+          <Box 
+            position="absolute" 
+            top={{base:"100%",md:"10%"}} 
+            left={{base:"0%",md:"50%",lg:"70%"}} 
+            width= {{base:"100%",md:"90%",lg:"70%"}} 
+            height="100%"
+            textAlign="end"
+            p={{base:"0% 5%", md:"0px"}}
+            backgroundColor="#00000000">
+
+            <p style={{fontSize:"12px"}}>Individual Project</p>
+            <Text fontSize="25px" color="#c9fff3">Youtube-clone</Text>
+            <Box 
+              m={{base:"10px 0px", sm:"5px 0px 0px 0px"}}
+              p="10px 20px" 
+              color="#64ffdb9a"
+              borderRadius="5px" 
+              boxShadow = "black 0px 20px 30px -10px"
+              backgroundColor="#001c29" 
+              >
+              <Text fontSize={{base:"13px",sm:"15px", md:"17px"}}>YouTube is a free, international, video sharing and social networking website and app on the internet.The website lets people upload, view, and share videos. <br />This project is developed in 2 days.</Text>
+              <br />
+            <Text color="#c9fff3" fontSize={{base:"13px",sm:"15px"}} ml="20%" > HTML | CSS | JavaScript (ES6+) | Youtube-api | LocalStorage</Text>
+            <br />
+            <Flex justifyContent="end" gap="10px" >
+              <a target="_blank" href="https://github.com/masai-course/agam_fw20_0819/tree/master/unit-3/sprint-4/yutube"><SiGithub color="#c9fff3" fontSize="20px" /></a>
+              <a target="_blank" href="https://chic-croissant-874b08.netlify.app/"><FaExternalLinkAlt color="#c9fff3" fontSize="20px" /></a>
+            </Flex>
+            </Box>
+    
+          </Box>
+        </Box>
+        {/* <Box position= "relative" width= {{base:"100%",md:"70%"}} height= {{base:"250px",md:"300px"}} mb={{base:"330px", md:"10%"}}  >
+          <Image src={trademart} objectFit="cover" w="100%" height={{base:"80%", sm:"100%"}} borderRadius="10px" boxShadow="black 0px 5px 15px"/>
+
+          <Box 
+            position="absolute" 
+            top={{base:"100%",md:"5%"}} 
+            left={{base:"0%",md:"50%",lg:"70%"}} 
+            width= {{base:"100%",md:"90%",lg:"70%"}} 
+            height="100%"
+            textAlign="end" 
+            p={{base:"0% 5%", md:"0px"}}
+            backgroundColor="#00000000">
+
+            <p style={{fontSize:"12px"}}>Group Project</p>
+            <Text fontSize="25px" color="#c9fff3">Indiamart clone</Text>
+            <Box 
+              m={{base:"10px 0px", sm:"5px 0px 30px 0px"}}
+              p="10px 20px" 
+              color="#64ffdb9a"
+              borderRadius="5px" 
+              boxShadow = "black 0px 20px 30px -10px"
+              backgroundColor="#001c29" 
+              >
+              <Text fontSize={{base:"13px",sm:"16px"}}>IndiaMART is an Indian e-commerce company that provides B2B and customer to customer sales services. <br />Collaborative project developed along with four team members in 5 days.</Text>
+              <br />
+            <Text color="#c9fff3" fontSize={{base:"13px",sm:"15px"}} ml="20%" >Redux | React | Chakra UI | HTML | CSS | JavaScript (ES6+) | Firebase | json-server | Render | LocalStorage</Text>
+            <br />
+            <Flex justifyContent="end" gap="10px" >
+              <a target="_blank" href="https://github.com/jithstephen13/tested-request-4840"><SiGithub color="#c9fff3" fontSize="20px" /></a>
+              <a target="_blank" href="https://trademart.netlify.app/"><FaExternalLinkAlt color="#c9fff3" fontSize="20px" /></a>
+            </Flex>
+            </Box>
+    
+          </Box>
+        </Box> */}
+        {/* <Box position= "relative" width= {{base:"100%",md:"70%"}} height= {{base:"250px",md:"300px"}} mb={{base:"300px", md:"10%"}}  >
+          <Image src={acras} objectFit="cover" w="100%" height={{base:"80%", sm:"100%"}} borderRadius="10px" boxShadow="black 0px 5px 15px" />
+
+          <Box 
+            position="absolute" 
+            top={{base:"100%",md:"10%"}} 
+            left={{base:"0%", md:"50%",lg:"70%"}} 
+            width= {{base:"100%",md:"90%",lg:"70%"}} 
+            height="100%"
+            textAlign="end" 
+            p={{base:"0% 5%", md:"0px"}}
+            backgroundColor="#00000000">
+
+            <p style={{fontSize:"12px"}}>Group Project</p>
+            <Text fontSize="25px" color="#c9fff3">JCREW clone</Text>
+            <Box 
+              m={{base:"10px 0px", sm:"5px 0px 0px 0px"}}
+              p="10px 20px" 
+              color="#64ffdb9a"
+              borderRadius="5px" 
+              boxShadow = "black 0px 20px 30px -10px"
+              backgroundColor="#001c29" 
+              >
+              <Text fontSize={{base:"13px",sm:"16px"}}>An international clothing brand. This offers an assortment of women's, men's, and children's apparel and accessories. <br />Collaborative project developed along with four team members in 5 days.</Text>
+              <br />
+            <Text color="#c9fff3" fontSize={{base:"13px",sm:"15px"}} >HTML | CSS | JavaScript (ES6+) | LocalStorage</Text>
+            <br />
+            <Flex justifyContent="end" gap="10px" >
+              <a target="_blank" href="https://github.com/Dante2109/JCrew_clone"><SiGithub color="#c9fff3" fontSize="20px" /></a>
+              <a target="_blank" href="https://acras.netlify.app/"><FaExternalLinkAlt color="#c9fff3" fontSize="20px" /></a>
+            </Flex>
+            </Box>
+    
+          </Box>
+        </Box> */}
+        {/* <Box position= "relative" width= {{base:"100%",md:"70%"}} height= {{base:"250px",md:"300px"}} mb={{base:"300px", md:"10%"}}  >
+          <Image src={cricbuzz} objectFit="cover" w="100%" height={{base:"80%", sm:"100%"}} borderRadius="10px" boxShadow="black 0px 5px 15px" />
+
+          <Box 
+            position="absolute" 
+            top={{base:"100%",md:"10%"}} 
+            left={{base:"0%",md:"50%",lg:"70%"}} 
+            width= {{base:"100%",md:"90%",lg:"70%"}} 
+            height="100%"
+            textAlign="end" 
+            p={{base:"0% 5%", md:"0px"}}
+            backgroundColor="#00000000">
+
+            <p style={{fontSize:"12px"}}>Group Project</p>
+            <Text fontSize="25px" color="#c9fff3">CricBuzz</Text>
+            <Box 
+              m={{base:"10px 0px", sm:"5px 0px 0px 0px"}}
+              p="10px 20px" 
+              color="#64ffdb9a"
+              borderRadius="5px" 
+              boxShadow = "black 0px 20px 30px -10px"
+              backgroundColor="#001c29" 
+              >
+              <Text fontSize={{base:"13px",sm:"16px"}}>Cricbuzz is an Indian cricket news website. It features, news, articles and live coverage of cricket matches including videos, text commentary. <br />This project is developed in 5 days.</Text>
+              <br />
+            <Text color="#c9fff3" fontSize={{base:"13px",sm:"15px"}} >HTML | CSS | JavaScript (ES6+) | LocalStorage | Embedded-videos</Text>
+            <br />
+            <Flex justifyContent="end" gap="10px" >
+              <a target="_blank" href="https://github.com/swarup-1/CricBuzz-Clone"><SiGithub color="#c9fff3" fontSize="20px" /></a>
+              <a target="_blank" href="https://web101project.netlify.app/"><FaExternalLinkAlt color="#c9fff3" fontSize="20px" /></a>
+            </Flex>
+            </Box>
+          </Box> */}
+        {/* </Box> */}
+      </Box>
+  </Box>
   )
 }
 
